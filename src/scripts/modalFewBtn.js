3c0f5415ccbb49e064d85a6715f2092428332b60
VladaBoiko <102071674+VladaBoiko@users.noreply.github.com>
2023-04-08 15:34:08 +0300

    add base

--- src/scripts/modalFewBtn.js
+++ src/scripts/modalFewBtn.js
@@ -1,14 +1,22 @@
 (() => {
   const refs = {
-    openModalBtnHero: document.querySelector('[data-modal-open-hero]'), //використовуємо зрозумілі, прості, унікальні селектори, в яких не заплутаємося
-    openModalBtnSectionFirst: document.querySelector('[data-modal-open-first]'), //використовуємо зрозумілі, прості, унікальні селектори, в яких не заплутаємося
-    openModalBtnSectionSecond: document.querySelector('[data-modal-open-second]'), //використовуємо зрозумілі, прості, унікальні селектори, в яких не заплутаємося
-    openModalBtnSectionThird: document.querySelector('[data-modal-open-third]'), //використовуємо зрозумілі, прості, унікальні селектори, в яких не заплутаємося
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
+    // openModalBtnHero: document.querySelector('[data-modal-open-hero]'), //використовуємо зрозумілі, прості, унікальні селектори, в яких не заплутаємося
+    // openModalBtnSectionFirst: document.querySelector('[data-modal-open-first]'), //використовуємо зрозумілі, прості, унікальні селектори, в яких не заплутаємося
+    // openModalBtnSectionSecond: document.querySelector(
+    //   '[data-modal-open-second]'
+    // ),
+    //використовуємо зрозумілі, прості, унікальні селектори, в яких не заплутаємося
+    // openModalBtnSectionThird: document.querySelector('[data-modal-open-third]'), //використовуємо зрозумілі, прості, унікальні селектори, в яких не заплутаємося
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.forEach(el => {
+    el.addEventListener('click', toggleModal);
+  });
+  // refs.openModalBtnHero.addEventListener('click', toggleModal);
+  // refs.openModalBtnSectionFirst.addEventListener('click', toggleModal);
+  // refs.openModalBtnSectionSecond.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
